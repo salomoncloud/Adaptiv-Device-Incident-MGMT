47bc4f75c7c0c6b345acc90d424dc4c0aa1a480e
Salomon <158000174+salomoncloud@users.noreply.github.com>
2025-06-30 15:07:37 -0400

    Update outputs.tf

--- cpe-tracker-webapp/terraform/modules/s3_frontend/outputs.tf
+++ cpe-tracker-webapp/terraform/modules/s3_frontend/outputs.tf
@@ -1,3 +1,13 @@
 output "website_url" {
   value = aws_s3_bucket_website_configuration.frontend.website_endpoint
 }
+
+output "bucket_domain_name" {
+  description = "S3 bucket domain name for CloudFront origin"
+  value       = aws_s3_bucket.frontend.bucket_domain_name
+}
+
+output "bucket_arn" {
+  description = "S3 bucket ARN"
+  value       = aws_s3_bucket.frontend.arn
+}
