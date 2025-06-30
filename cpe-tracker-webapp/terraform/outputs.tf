42a99cf4e8af41d2a76a0e0419f1899633c167df
Salomon <158000174+salomoncloud@users.noreply.github.com>
2025-06-30 15:09:10 -0400

    Update outputs.tf

--- cpe-tracker-webapp/terraform/outputs.tf
+++ cpe-tracker-webapp/terraform/outputs.tf
@@ -4,8 +4,33 @@ output "api_endpoint" {
 }
 
 output "website_url" {
-  description = "S3 website URL"
+  description = "Website URL with custom domain"
+  value       = module.cloudfront.website_url
+}
+
+output "s3_website_url" {
+  description = "S3 website URL (fallback)"
   value       = module.s3_frontend.website_url
+}
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront Distribution ID"
+  value       = module.cloudfront.cloudfront_distribution_id
+}
+
+output "cloudfront_domain_name" {
+  description = "CloudFront Distribution Domain Name"
+  value       = module.cloudfront.cloudfront_domain_name
+}
+
+output "route53_zone_id" {
+  description = "Route 53 Hosted Zone ID"
+  value       = module.cloudfront.route53_zone_id
+}
+
+output "route53_name_servers" {
+  description = "Route 53 Name Servers - Update your domain registrar with these"
+  value       = module.cloudfront.route53_name_servers
 }
 
 output "cognito_user_pool_id" {
@@ -21,4 +46,9 @@ output "cognito_client_id" {
 output "frontend_bucket_name" {
   description = "S3 bucket name for frontend"
   value       = var.frontend_bucket_name
+}
+
+output "acm_certificate_arn" {
+  description = "ACM Certificate ARN"
+  value       = module.cloudfront.acm_certificate_arn
 }
